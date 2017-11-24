af6fcccd749d955750f0407a461165cfcc6393ef
Ishan Yapa <ishanyapa@gmail.com>
2017-11-25 04:41:42 +0530

    update account ui implemented

--- app/payment-mgt/payment-mgt.helper.js
+++ app/payment-mgt/payment-mgt.helper.js
@@ -4,10 +4,9 @@ angular.module('cts.payment-mgt')
 
     .factory('Payment',['$http', function ($http) {
 
-        const payementFactory = {};
+        const paymentFactory = {};
 
-
-        payementFactory.getAccountDetails = (userId) =>{
+        paymentFactory.getAccountDetails = (userId) =>{
 
             // Get logged user details by sending user ID
             return $http.get("https://cyborgs-ts-payment-service.herokuapp.com/accounts/".concat(userId)).then( (response) => {
@@ -17,5 +16,25 @@ angular.module('cts.payment-mgt')
             });
         };
 
-        return payementFactory;
+        paymentFactory.validateCard = (payload) => {
+
+            // validate card when details are given
+            return $http.post('http://localhost:9803/cards/validate', payload).then((response) => {
+
+                return response;
+
+            })
+        }
+
+        paymentFactory.updateBalance = (payload) => {
+
+            // update the account balance when details are given
+            return $http.put('https://cyborgs-ts-payment-service.herokuapp.com/accounts/balance', payload).then((response) => {
+
+                return response;
+            })
+
+        }
+
+        return paymentFactory;
     }])
